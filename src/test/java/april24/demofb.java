d31cd102cfc9f1702b495ba957a4b0ef9e69b216
vishn <pcjishnu492@gmail.com>
2024-08-19 12:57:04 +0530

    first commit

---
+++ src/test/java/april24/demofb.java
@@ -0,0 +1,34 @@
+package april24;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class demofb {
+	WebDriver driver;
+	By fbemail=By.id("email");
+	By.fbpassword=By.id("pass");
+	By.fblogin=By.name("login");
+	
+	public demofb (WebDriver driver)
+	
+	{
+		this.driver=driver;
+	}
+	public void setvalues(String email,String password)
+	{
+		driver.findElement(fbemail).clear();
+		driver.findElement(fbemail).sendKeys(email);
+		driver.findElement(fbpassword).clear();
+		driver.findElement(fbpassword).sendkeys(password);
+	}
+	
+	public void login()
+	{
+		driver.findElement(fblogin).click();
+	}
+	
+	
+	
+	
+
+}
